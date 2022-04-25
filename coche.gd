12dc4d168ac5f2347b0cf45fa6212160aed79a90
raimon <101257136+raimonlopezg@users.noreply.github.com>
2022-04-25 21:16:49 +0200

    coche enrera
    ara es pot posar una marcha atras pero no va molt be :) :+1:

--- coche.gd
+++ coche.gd
@@ -1,21 +1,23 @@
 extends KinematicBody2D
 
-
 var velocitat := Vector2.ZERO
 var acceleracio := Vector2.ZERO
 var direccio := Vector2.RIGHT
 var velocitat_max := 150
 var velocitat_zero = 0
-func _physics_process(delta):
-	if Input.is_action_pressed("W"):
+func _physics_process(delta) :
+	if Input.is_action_pressed("W") :
 		acceleracio = direccio * 75
-	else:
+	else :
 		velocitat = lerp(velocitat, Vector2.ZERO, 2/(velocitat.length()+1))
-
+		
+	if Input.is_action_pressed("S") :
+		acceleracio = direccio * -75
+	
 	velocitat += acceleracio * delta
 	if velocitat.length() > velocitat_max :
 		velocitat = velocitat.normalized() * velocitat_max
 	
 	velocitat =  move_and_slide(velocitat)
-	if not velocitat.is_equal_approx(Vector2.ZERO):
+	if not velocitat.is_equal_approx(Vector2.ZERO) :
 		direccio = velocitat.normalized()
