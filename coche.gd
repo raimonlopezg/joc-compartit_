31dd1a54f05a64123b6a424a7ffde88f1f60a77f
raimon <101257136+raimonlopezg@users.noreply.github.com>
2022-04-26 23:30:28 +0200

    pit stops
    pit stops, marcha enrera numero de voltes, linea de meta

--- coche.gd
+++ coche.gd
@@ -1,18 +1,20 @@
-extends KinematicBody2D
+ extends KinematicBody2D
 
 var velocitat := Vector2.ZERO
 var acceleracio := Vector2.ZERO
 var direccio := Vector2.RIGHT
 var velocitat_max := 150
 var velocitat_zero = 0
+var num_voltes = -1  
 func _physics_process(delta) :
 	if Input.is_action_pressed("W") :
 		acceleracio = direccio * 75
+	
 	else :
 		velocitat = lerp(velocitat, Vector2.ZERO, 2/(velocitat.length()+1))
 		
 	if Input.is_action_pressed("S") :
-		acceleracio = direccio * -75
+		acceleracio = direccio * -50
 	
 	velocitat += acceleracio * delta
 	if velocitat.length() > velocitat_max :
@@ -21,3 +23,8 @@ func _physics_process(delta) :
 	velocitat =  move_and_slide(velocitat)
 	if not velocitat.is_equal_approx(Vector2.ZERO) :
 		direccio = velocitat.normalized()
+
+
+func _on_area_meta_body_entered(body):
+	num_voltes = num_voltes + 1 
+	print ("portes ",num_voltes," voltes acabades")
