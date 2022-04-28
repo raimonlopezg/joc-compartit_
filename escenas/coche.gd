cc638afe63bf0eca475334028d0b133d90099653
alexgrandess <agrandesginer@gmail.com>
2022-04-28 08:48:34 +0200

    codi refresh
    el codi s'ha fet un altre cop i va millor

--- escenas/coche.gd
+++ escenas/coche.gd
@@ -1,46 +1,41 @@
  extends KinematicBody2D
 
-var velocitat := Vector2.ZERO
-var acceleracio := Vector2.ZERO
+var velocitat := 0
+var acceleracio := 0
 var direccio := Vector2.RIGHT
 var velocitat_max := 110
 var velocitat_zero = 0
-var num_voltes = -2
-var velocitat_gir = 150
-
+var num_voltes = -1
+var velocitat_gir_min = 200
 var angle = 0
 
-func _physics_process(delta) :
+#vel_min  velocitat_gir = 200
+#vel_max  velocitat_gir = 80
 
+func _physics_process(delta) :
 	if Input.is_action_pressed("W") :
-		acceleracio = direccio * 65
+		acceleracio = 65
 	else :
-		velocitat = direccio * lerp(velocitat.length(), 0, 2/(velocitat.length()+1))
-		
+		velocitat = lerp(velocitat, 0, 2/(velocitat + 1))
 	if Input.is_action_pressed("S") :
-		acceleracio = direccio * -30
-	
+		acceleracio = -30
 	if Input.is_action_pressed("A"):
-		velocitat = velocitat.rotated(-deg2rad(velocitat_gir) * delta)
+		direccio = direccio.rotated(-deg2rad(-1.09*velocitat + velocitat_gir_min) * delta)
 	if Input.is_action_pressed("D"):
-		velocitat = velocitat.rotated(deg2rad(velocitat_gir) * delta)
-		
+		direccio = direccio.rotated(deg2rad(-1.09*velocitat + velocitat_gir_min) * delta)
 	
 	velocitat += acceleracio * delta
-	if velocitat.length() > velocitat_max :
-		velocitat = velocitat.normalized() * velocitat_max
-	
-	velocitat =  move_and_slide(velocitat)
-	rotation = velocitat.angle()
-	
-	if not velocitat.is_equal_approx(Vector2.ZERO) :
-		direccio = velocitat.normalized()
+	if velocitat > velocitat_max :
+		velocitat = velocitat_max
+	var moviment =  move_and_slide(velocitat * direccio)
+	velocitat = moviment.length()
+	if not moviment.is_equal_approx(Vector2.ZERO) :
+		direccio = moviment.normalized()
+	rotation = direccio.angle()
 	
 func _on_meta_body_entered(body):
 	num_voltes = num_voltes + 1 
 	print ("portes ",num_voltes," voltes acabades")
-
-
 func _on_boxes_body_exited(body):
 	velocitat_max = 110
 func _on_boxes_body_entered(body):
